853f3928b57980b77abb1b5c781678fee025830a
Arseniy Pelevin <arsenii.pelevin@campus.lmu.de>
2026-01-20 12:01:40 +0100

    Update datalad run message in datalad_wapper

--- code/datalad_wrapper.sh
+++ code/datalad_wrapper.sh
@@ -9,7 +9,7 @@ log_file="logs/params_table.log"
 
 echo "START $(date)" >> "$log_file"
 
-"$DL" run -m "Create files by stimulus types" \
+"$DL" run -m "Generate presentations.csv properly concatenated between session types" \
   "bash -lc '$PY code/0_access_stim_params.py'" >> logs/datalad.log 2>&1
 
 echo "END $(date) EXIT:$?" >> "$log_file"
