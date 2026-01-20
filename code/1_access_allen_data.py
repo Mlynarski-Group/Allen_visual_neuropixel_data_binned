2428f613ee4cebb82d0c416b010073db4daefe31
Arseniy Pelevin <arsenii.pelevin@campus.lmu.de>
2026-01-20 21:18:54 +0100

    Edit 1_access_allen_data.py to use prefiltered presentations

--- code/1_access_allen_data.py
+++ code/1_access_allen_data.py
@@ -39,6 +39,9 @@ relevant_stimulus_parameters = {
 }
 
 invalid_stimulus_substr = ['spontaneous', 'shuffled', 'invalid']
+filtered_presentations_path = Path(
+    "data/presentations/03_presentations_filtered_by_duration.csv"
+)
 
 completed_sessions_path = Path("logs/completed_sessions")
 completed_sessions = []
@@ -47,49 +50,60 @@ if completed_sessions_path.exists():
         completed_sessions = [int(l.strip()) for l in f if l.strip()]
 
 
-def process_session(cache, session_id):
+def process_session(cache, session_id, presentations_by_session):
     session = cache.get_session_data(session_id)
     log.info("Session %s loaded.", session_id)
 
-    # Access stimulus presentations
-    stimulus_types = session.stimulus_names
-    # Filter out unwanted stimulus types
+    session_presentations = presentations_by_session[session_id]
+
+    stimulus_types = session_presentations['stimulus_name'].unique().tolist()
     stimulus_types = [st for st in stimulus_types
                       if all(substr not in st for substr in invalid_stimulus_substr)]
 
     running_speed = session.running_speed
     gaze_data = session.get_screen_gaze_data()
     log.info('Obtained running speed and gaze data.')
+    unit_ids = session.units.index.values
 
-    output_folder = Path(f'data/01_sessions_presentations/session_{session_id}/')
+    output_folder = Path(f'logs/data/01_sessions_presentations/session_{session_id}/')
     output_folder.mkdir(parents=True, exist_ok=True)
 
     for stimulus_type in stimulus_types:
         log.info("\tProcessing stimulus type: %s", stimulus_type)
         process_stimulus_type(
-            stimulus_type, session, output_folder, running_speed, gaze_data)
+            stimulus_type,
+            session,
+            unit_ids,
+            session_presentations,
+            output_folder,
+            running_speed,
+            gaze_data,
+        )
 
     # Free memory
     del session
     gc.collect()
 
-def process_stimulus_type(stimulus_type, session, output_folder,
-                          running_speed, gaze_data):
-    type_presentations = get_type_presentations(stimulus_type, session)
+def process_stimulus_type(stimulus_type, session, unit_ids, session_presentations,
+                          output_folder, running_speed, gaze_data):
+    type_presentations = get_type_presentations(
+        stimulus_type, session_presentations)
+    if type_presentations.empty:
+        log.warning("\t\tNo presentations of type %s.", stimulus_type)
+        return
 
     # Compute spike counts
-    spike_counts = compute_spike_counts(type_presentations, session)
-    log.info('\t\tComputed spike counts.')
+    is_natural_movie = 'natural_movie' in stimulus_type
+    spike_counts = compute_spike_counts(
+        type_presentations, session, unit_ids, apply_clip=is_natural_movie)
     # Get running speed and gaze data aligned to presentations
     type_running_speed = get_running_speed(
         spike_counts, running_speed, type_presentations)
-    log.info('\t\tComputed running speed.')
     if gaze_data is not None:
         type_gaze_data = get_gaze_data(
             spike_counts, gaze_data, type_presentations)
     else:
         type_gaze_data = None
-    log.info('\t\tComputed gaze data.')
 
     stimuli = type_presentations.astype(np.float32).to_xarray()
 
@@ -102,18 +116,24 @@ def process_stimulus_type(stimulus_type, session, output_folder,
         output_folder
     )
 
-def get_type_presentations(stimulus_type, session):
-    # Access stimulus presentations of the given type
-    type_presentations = session.get_stimulus_table(stimulus_type)
+def get_type_presentations(stimulus_type, session_presentations):
+    type_presentations = session_presentations[
+        session_presentations['stimulus_name'] == stimulus_type
+    ]
 
     # Keep only relevant columns
-    stimulus_columns = (relevant_stimulus_parameters[stimulus_type] +
-                        ['stimulus_block', 'start_time', 'stop_time', 'duration'])
-    type_presentations = type_presentations[stimulus_columns]
+    params = relevant_stimulus_parameters[stimulus_type]
+    stimulus_columns = (params + [
+        'stimulus_presentation_id',
+        'stimulus_block',
+        'start_time',
+        'stop_time',
+        'duration',
+    ])
 
-    # Group movie frame presentations into whole movie presentations
-    if 'movie' in stimulus_type:
-        type_presentations = whole_movie_presentations(type_presentations)
+    type_presentations = type_presentations[stimulus_columns]
+    type_presentations = type_presentations.set_index(
+        'stimulus_presentation_id', drop=True)
 
     # Filter out null values for drifting or static gratings
     if 'gratings' in stimulus_type or 'dot_motion' in stimulus_type:
@@ -126,43 +146,27 @@ def get_type_presentations(stimulus_type, session):
 
     return type_presentations
 
-def whole_movie_presentations(presentations):
-    df = presentations.reset_index()
-    frames = df['frame']
-    new_movie = frames.diff().fillna(1) < 0
-    df['movie_group'] = new_movie.cumsum()
-    agg = df.groupby('movie_group').agg(
-        stimulus_block=('stimulus_block', 'first'),
-        start_time=('start_time', 'min'),
-        stop_time=('stop_time', 'max'),
-        first_presentation_id=('stimulus_presentation_id', 'first'),
-    )
-    agg['duration'] = agg['stop_time'] - agg['start_time']
-    agg = agg.set_index('first_presentation_id')
-    agg.index.name = 'stimulus_presentation_id'
-    return agg
+def compute_spike_counts(presentations, session, unit_ids, bin_width=0.02,
+                         apply_clip=True):
 
-def compute_spike_counts(presentations, session, bin_width = 0.02):
-
-    # Crate bins including end time
+    # Create bins including end time
     max_dur = presentations['duration'].max()
     n_bins = int(np.ceil(max_dur / bin_width))
     time_bin_edges = np.arange(0, (n_bins + 1) * bin_width, bin_width)
 
-    # Only count spikes up to the stop time of each presentation
-    stop_times = presentations['stop_time'].values
-    clip_cb = lambda domain: np.minimum(domain, stop_times[:, None])
-
     # Construct 3D array of binarized spike counts
     ids = presentations.index.values
-    unit_ids = session.units.index.values
-    spike_counts = session.presentationwise_spike_counts(
-        bin_edges=time_bin_edges,
-        stimulus_presentation_ids=ids,
-        unit_ids=unit_ids,
-        binarize=True,
-        time_domain_callback=clip_cb
-        )
+    kwargs = {
+        "bin_edges": time_bin_edges,
+        "stimulus_presentation_ids": ids,
+        "unit_ids": unit_ids,
+        "binarize": True,
+    }
+    if apply_clip:
+        stop_times = presentations['stop_time'].values
+        clip_cb = lambda domain: np.minimum(domain, stop_times[:, None])
+        kwargs["time_domain_callback"] = clip_cb
+    spike_counts = session.presentationwise_spike_counts(**kwargs)
 
     return spike_counts
 
@@ -219,10 +223,6 @@ def combine_and_save_data(
         gaze_data.to_netcdf(output_folder / f'{stimulus_type}_gaze_data.nc')
     stimuli.to_netcdf(output_folder / f'{stimulus_type}_stimuli.nc')
 
-    log.info("\t\tFiles for stimulus type %s saved.", stimulus_type)
-
-    # combined_data.to_netcdf(output_folder / f'{stimulus_type}.nc')
-
 def main():
     # Define working folder and load the cache
     input_dir = '/storage2/wp7/allendata'
@@ -230,22 +230,27 @@ def main():
     cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
     log.info("Cache loaded.")
 
-    # Load sessions_table
     sessions_table = cache.get_session_table()
     session_ids = sessions_table.index.values.tolist()
     log.info("Found %d sessions.", len(session_ids))
 
+    presentations = pd.read_csv(filtered_presentations_path)
+    presentations_by_session = {
+        int(session_id): df
+        for session_id, df in presentations.groupby('session_id', sort=False)
+    }
+
     # Process each session
-    for session_id in session_ids:
+    for idx, session_id in enumerate(session_ids, start=1):
         # Skip already completed session
         if session_id in completed_sessions:
             log.info("Skipping already completed session %s.", session_id)
             continue
 
         log.info("Processing session %s (%d/%d)",
-            session_id, session_ids.index(session_id)+1, len(session_ids))
+            session_id, idx, len(session_ids))
         t0 = time.perf_counter()
-        process_session(cache, session_id)
+        process_session(cache, session_id, presentations_by_session)
         t1 = time.perf_counter()
         dur = t1 - t0
         log.info("Finished session %s in %.2f seconds.", session_id, dur)
