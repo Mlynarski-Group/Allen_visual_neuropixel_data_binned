845d13519d11661c8aa35e4a0e23af621e60014a
Arseniy Pelevin <arsenii.pelevin@campus.lmu.de>
2026-01-20 13:51:24 +0100

    Organize code for presentations

---
+++ code/presentations/02_whole_movies.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+
+def whole_movie_presentations(df):
+    frames = df['frame']
+    new_movie = frames.diff().fillna(1) < 0
+    df.loc[:, 'movie_group'] = new_movie.cumsum()
+    agg = df.groupby(['session_id', 'movie_group', 'stimulus_name']).agg(
+        stimulus_presentation_id=('stimulus_presentation_id', 'first'),
+        stimulus_block=('stimulus_block', 'first'),
+        start_time=('start_time', 'min'),
+        stop_time=('stop_time', 'max'),
+        duration=('duration', 'sum')
+    )
+    return agg
+
+input_path = 'data/presentations/01_presentations_original.csv'
+output_path = 'data/presentations/02_presentations_whole_movies.csv'
+
+pres = pd.read_csv(input_path)
+movie_pres = pres[pres['stimulus_name'].str.contains('natural_movie')]
+
+movie_pres_whole = whole_movie_presentations(movie_pres)
+movie_pres_whole = movie_pres_whole.reset_index().drop(columns=['movie_group'])
+
+other_pres = pres[~pres['stimulus_name'].str.contains('natural_movie')]
+
+all_pres = pd.concat([other_pres, movie_pres_whole], ignore_index=True)
+all_pres = all_pres.sort_values(['session_id', 'stimulus_presentation_id'])
+
+all_pres.to_csv(output_path, index=False)
